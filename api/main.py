0828655197c0e4714f77e4bbda431680d6c35f3c
Michu M <michal.mmtk@gmail.com>
2021-12-11 23:05:19 +0100

    Adjust api for frontend development

--- api/main.py
+++ api/main.py
@@ -1,21 +1,53 @@
 import uvicorn
 from fastapi import FastAPI
-from api.routers import proposal, unit
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Optional
+# from api.routers import proposal, unit
 
 app = FastAPI()
-app.include_router(proposal.router, prefix="/proposal", tags=["proposals"])
-app.include_router(unit.router, prefix="/unit", tags=["units"])
+# app.include_router(proposal.router, prefix="/proposal", tags=["proposals"])
+# app.include_router(unit.router, prefix="/unit", tags=["units"])
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/test")
 async def test():
     return {"message": "test"}
 
+class Item(BaseModel):
+    name: str
+    price: float
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+
+@app.post("/")
+async def receiveMessage(message: dict) -> dict:
+    print(message['message'])
+    return {
+        "data": { "Message received." }
+    }
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info", reload=True)
+    uvicorn.run("main:app", host="localhost", port=8080, log_level="info", reload=True)

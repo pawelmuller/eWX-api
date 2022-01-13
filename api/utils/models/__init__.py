7cea5e9e17fa44b7bbe8d3327a444f3d37a33616
Paweł Müller <pawelmuller42@gmail.com>
2022-01-14 00:48:18 +0100

    EW4-25| Added common response model for creating entities.

--- api/utils/models/__init__.py
+++ api/utils/models/__init__.py
@@ -1,4 +1,5 @@
 from api.utils.models.advances import AdvanceModel
+from api.utils.models.common import CreateEntityResponseModel
 from api.utils.models.expenses import ExpenseModel
 from api.utils.models.funding_sources import FundingSourcesModel
 from api.utils.models.proposals import CreateProposalRequestModel, CreateProposalCommentRequestModel
